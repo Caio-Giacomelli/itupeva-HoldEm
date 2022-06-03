3393b970cf5875a0fa4bbfbbeb402dfb036e5c32
Caio-Giacomelli <caio.h.giacomelli@gmail.com>
2022-06-02 23:28:52 -0300

    DEV - Implement Shuffle Algorithm

--- Assets/Scripts/Controllers/ShuffleController.cs
+++ Assets/Scripts/Controllers/ShuffleController.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class ShuffleController : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void shuffleCards(){
-        Card card = new Card(Suit.SPADES, Value.ACE);
-        Debug.Log(card.value);
+        Deck deck = new Deck();
+        System.Random rng = new System.Random();
+        Card[,] generatedDeck = deck.cards;
+
+        printDeck(generatedDeck, "before ");
+
+        int lengthRow = generatedDeck.GetLength(1);
+        for (int i = generatedDeck.Length - 1; i > 0; i--){
+            int i0 = i / lengthRow;
+            int i1 = i % lengthRow;
+
+            int j = rng.Next(i + 1);
+            int j0 = j / lengthRow;
+            int j1 = j % lengthRow;
+
+            Card temp = generatedDeck[i0, i1];
+            generatedDeck[i0, i1] = generatedDeck[j0, j1];
+            generatedDeck[j0, j1] = temp;
+        }
+
+        printDeck(generatedDeck, "after ");
+    }
+
+    private void printDeck(Card[,] generatedDeck, string message){
+        int NUM_SUITS = System.Enum.GetValues(typeof(Suit)).Length;
+        int NUM_VALUES = System.Enum.GetValues(typeof(Value)).Length;
+        
+        for (int suitIndex = 0; suitIndex < NUM_SUITS; suitIndex++) {
+            for (int valueIndex = 0; valueIndex < NUM_VALUES; valueIndex++) {
+                Debug.Log(message + "" + generatedDeck[suitIndex, valueIndex].value);
+                Debug.Log(message + "" + generatedDeck[suitIndex, valueIndex].suit);
+            }
+        }
     }
 }
